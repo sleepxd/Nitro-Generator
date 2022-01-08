1a845b7df7cb766769abe302dd9c10824b7c9b56
aminXD <dr.m.a.parsaei@gmail.com>
2022-01-08 20:32:03 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# <center>✨Nitro generator, but its ***improved***✨</center>
+
+![benchmark with 10,00 code](benchmark/benchmark.png)
+
+## changes
+
+i just used the raw choicer algorithm instead of `random.choice()` and added threading support, for huge performance boost.
+
+### closing apps can help a lot for more performance
+
+---
+
+star/fork if u liked it! ❤
--- nitrogen.py
+++ nitrogen.py
@@ -1,12 +1,30 @@
-import random, string
+"""nitro generator, but its improved"""
+import random
+import string
+import time
+import threading
 from colorama import init, Fore
+
+ALPHABETS = string.ascii_letters + string.digits
 
 init(convert=True)
 
-print('%sCodes:%s ' % (Fore.CYAN, Fore.WHITE), end='')
-amount = int(input())
-for i in range(amount):
-    code = "https://discordapp.com/gifts/%s" % (('').join(random.choices(string.ascii_letters + string.digits, k=16)))
-    print('Code: %s' % (code))
-    with open('codes.txt', 'a') as f:
-        f.write('%s\n' % (code))
+def algorithm():
+    return "".join([ALPHABETS[int(random.random() * len(ALPHABETS))] for _ in range(16)])
+def generator(amount):
+    global CODES
+    CODES = []
+    for _ in range(amount):
+        code = "https://discordapp.com/gifts/%s" % algorithm()
+        print(f"Code: {code}")
+        CODES.append(code)
+
+print("%sCodes:%s " % (Fore.CYAN, Fore.WHITE), end="")
+count = int(input())
+start = time.time()
+thread = threading.Thread(target=generator,args=(count,))
+thread.start()
+thread.join()
+end = round(time.time() - start, 3)
+print(f"took {end}s to run")
+with open("codes.txt","w") as f: f.write("\n".join(CODES))
